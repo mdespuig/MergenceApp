54b4b6cd4f8bd16225af52a13f4e9217825c2bbb
Matt Despuig <mdespuig@gbox.adnu.edu.ph>
2025-05-21 22:38:45 +0800

    v1.3 (localhost)

---
+++ v1.3/mergence/asgi.py
@@ -0,0 +1,16 @@
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import mergenceApp.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mergence.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            mergenceApp.routing.websocket_urlpatterns
+        )
+    ),
+})
